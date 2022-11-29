c41c5b1a87a291fb4cf1e7179da17b564225178c
Khanh Duy Nguyen <55223995+duynguyen2001@users.noreply.github.com>
2022-11-29 00:19:17 -0600

    button added

---
+++ Stitchbox/Resources/DesignRules/Font.swift
@@ -0,0 +1,49 @@
+////
+////  Font.swift
+////  Stitchbox
+////
+////  Created by Khanh Duy Nguyen on 11/19/22.
+////
+//
+//import SwiftUI
+//
+//@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+//extension UIFont {
+//    static let Header1: UIFont = UIFont(name: "Fenwick", size: 64) ?? { return
+//        fatalError("""
+//            Failed to load the "Fenwick" font.
+//            Make sure the font file is included in the project and the font name is spelled correctly.
+//            """
+//        )}()
+//    static let Header2: UIFont = UIFont(name: "Helvetica-Neue", size: 75) ?? { return
+//        fatalError("""
+//            Failed to load the "Helvetica-Neue" font.
+//            Make sure the font file is included in the project and the font name is spelled correctly.
+//            """
+//        )}()
+//    static let Header3: UIFont = UIFont(name: "Helvetica-Neue", size: 65) ?? { return
+//        fatalError("""
+//            Failed to load the "Helvetica-Neue" font.
+//            Make sure the font file is included in the project and the font name is spelled correctly.
+//            """
+//        )}()
+//    static let Header4: UIFont = UIFont(name: "Helvetica-Neue", size: 55) ?? { return
+//        fatalError("""
+//            Failed to load the "Helvetica-Neue" font.
+//            Make sure the font file is included in the project and the font name is spelled correctly.
+//            """
+//        )}()
+//    static let Header5: UIFont = UIFont(name: "Helvetica-Neue-Light", size: 45) ?? { return
+//        fatalError("""
+//            Failed to load the "Helvetica-Neue" font.
+//            Make sure the font file is included in the project and the font name is spelled correctly.
+//            """
+//        )}()
+//    static let Text: UIFont = UIFont(name: "Helvetica-Neue", size: 45) ?? { return
+//        fatalError("""
+//            Failed to load the "Helvetica-Neue" font.
+//            Make sure the font file is included in the project and the font name is spelled correctly.
+//            """
+//        )}()
+//    static let sectionFooter: Font = Font.footer.italic()
+//}
