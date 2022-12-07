f0363978e832797e6b46cd2b79761fc703064d0d
Khanh Duy Nguyen <55223995+duynguyen2001@users.noreply.github.com>
2022-12-06 21:10:48 -0600

    add 2 login by phone and login by username

--- Stitchbox/StyledComponents/LoginTabBarController.swift
+++ Stitchbox/StyledComponents/LoginTabBarController.swift
@@ -19,6 +19,7 @@ import SwiftUI
     }
     
     func setupView() {
+        self.navigationItem.hidesBackButton = true
         // fixing position of tab bar
         let width = CGFloat(300)
         let height = CGFloat(30)
@@ -40,7 +41,7 @@ import SwiftUI
         let x = screenCenterX - width/2.0
         
         // y position of blue view, top half of the blue view is on top of the y center point
-        let y = screenCenterY - height/2.0 - CGFloat(300)
+        let y = screenCenterY - height/2.0 - CGFloat(200)
         self.tabBar.frame = CGRect(x: x, y: y, width: width, height: height)
         self.view.backgroundColor = .clear
         
