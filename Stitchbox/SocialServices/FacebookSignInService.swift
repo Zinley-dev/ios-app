bfb6ed76f95e3d870ddba93c5c4bd53be4eac639
Nghiem Minh Hoang <hoangnm.dev@gmail.com>
2023-01-06 01:23:56 +0700

    update login fb

--- Stitchbox/SocialServices/FacebookSignInService.swift
+++ Stitchbox/SocialServices/FacebookSignInService.swift
@@ -21,17 +21,19 @@ extension FacebookSignInService: LoginCoordinatorProtocol {
         print("FACEBOOK LOGIN...")
         let loginManager = LoginManager()
         loginManager.logIn(permissions: ["public_profile"], from: vm.vc) { result, error in
-                    if let error = error {
-                        print("Encountered Erorr: \(error)")
-                    } else if let result = result, result.isCancelled {
-                        print("Cancelled")
-                    } else {
-                        print("Logged In")
-                        print(result)
-//                        let data = AuthResult(idToken: userId, providerID: nil, rawNonce: nil, accessToken: nil, name: "\(familyName) \(givenName)", email: email, phone: nil)
-//                        self.vm.completeSignIn(with: data)
-                    }
-                }
+            if let error = error {
+                print("Encountered Erorr: \(error)")
+            } else if let result = result, result.isCancelled {
+                print("Cancelled")
+            } else {
+                print("Logged In")
+                let userId = Profile.current?.userID
+                let email = Profile.current?.email ?? ""
+                let name = "\(Profile.current?.firstName ?? "") \(Profile.current?.lastName ?? "")"
+                let data = AuthResult(idToken: userId, providerID: nil, rawNonce: nil, accessToken: nil, name: name, email: email, phone: nil)
+                self.vm.completeSignIn(with: data)
+            }
+        }
     }
     
     func logout() {
