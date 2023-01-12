184f17b64ef4b1833740cdce85a5cf29dacbf657
Khanh Duy Nguyen <55223995+duynguyen2001@users.noreply.github.com>
2023-01-12 01:18:35 -0600

    update authrequire model and userdatasource model

--- Stitchbox/Models/UserDataSource.swift
+++ Stitchbox/Models/UserDataSource.swift
@@ -8,37 +8,37 @@
 import ObjectMapper
 
 enum UserGender : String {
-  case male   = "male"
-  case female = "female"
+    case male   = "male"
+    case female = "female"
 }
 
 class UserDataSource: Mappable {
-  
-  var userID :    String! = ""
-  var userName :  String! = ""
-  var email :     String! = ""
-  var phone :     String! = ""
-  var gender :    UserGender = .male
-  var avatarURL : String!  = "https://sgp1.digitaloceanspaces.com/dev.storage/6bab1242-88c5-4705-81e9-3a9e13c47d41.png"
-  var signinMethod : String! = ""
-  var socialId : String! = ""
-  
-  required init?(map: Map) {
-    //
-  }
-  
-  init() {
     
-  }
-  
-  func mapping(map: Map) {
-    userID      <- map["ID"]
-    userName    <- map["username"]
-    email       <- map["email"]
-    phone       <- map["phone"]
-    gender      <- map["gender"]
-    avatarURL   <- map["avatar"]
-    signinMethod   <- map["signinMethod"]
-    socialId   <- map["socialId"]
-  }
+    private(set) var userID :    String? = ""
+    private(set) var userName :  String = ""
+    private(set) var email :     String = ""
+    private(set) var phone :     String = ""
+    private(set) var gender :    UserGender = .male
+    private(set) var avatarURL : String  = "https://sgp1.digitaloceanspaces.com/dev.storage/6bab1242-88c5-4705-81e9-3a9e13c47d41.png"
+    private(set) var signinMethod : String = ""
+    private(set) var socialId : String = ""
+    
+    required init?(map: Map) {
+        //
+    }
+    
+    init() {
+        
+    }
+    
+    func mapping(map: Map) {
+        userID      <- map["ID"]
+        userName    <- map["username"]
+        email       <- map["email"]
+        phone       <- map["phone"]
+        gender      <- map["gender"]
+        avatarURL   <- map["avatar"]
+        signinMethod   <- map["signinMethod"]
+        socialId   <- map["socialId"]
+    }
 }
