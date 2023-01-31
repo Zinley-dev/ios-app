2043a0a350f92931471f048a38e943b74354dbfd
kn1026 <116099862+kn1026@users.noreply.github.com>
2023-01-31 00:04:11 -0600

    fixed auto layout for profile header

--- Stitchbox/Constant/globalFile.swift
+++ Stitchbox/Constant/globalFile.swift
@@ -240,3 +240,27 @@ extension UICollectionReusableView {
 func check_Url(host: String) -> Bool {
     return streaming_domain.contains { $0.domain.contains(host) }
 }
+
+extension UITextView {
+    
+    /**
+     Calculates if new textview height (based on content) is larger than a base height
+     
+     - parameter baseHeight: The base or minimum height
+     
+     - returns: The new height
+     */
+    func newHeight(withBaseHeight baseHeight: CGFloat) -> CGFloat {
+        
+        // Calculate the required size of the textview
+        let fixedWidth = frame.size.width
+        let newSize = sizeThatFits(CGSize(width: fixedWidth, height: .greatestFiniteMagnitude))
+        var newFrame = frame
+        
+        // Height is always >= the base height, so calculate the possible new height
+        let height: CGFloat = newSize.height > baseHeight ? newSize.height : baseHeight
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: height)
+        
+        return newFrame.height
+    }
+}
