11bf3e1de4fbd5865c755ff0677091490b3b9a11
kn1026 <116099862+kn1026@users.noreply.github.com>
2023-03-02 13:10:59 -0600

    update

--- Stitchbox/Controllers/MainDashboardVC/SearchVC/SearchViewController.swift
+++ Stitchbox/Controllers/MainDashboardVC/SearchVC/SearchViewController.swift
@@ -11,27 +11,9 @@ class SearchViewController: UIViewController {
 
   @IBOutlet weak var searchBar: UISearchBar!
   
-  override func viewDidLoad() {
-      super.viewDidLoad()
-    let searchField = searchBar.value(forKey: "searchField") as? UITextField
-
-    
-    if let field = searchField {
-      field.textColor = .white
-      field.backgroundColor = .darkGray
-      field.rightView?.tintColor = .white
-      field.leftView?.tintColor = .white
-      
-      let placeholderString = NSAttributedString(string: "Search", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-      field.attributedPlaceholder = placeholderString
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    
-    
-      searchBar.tintColor = .white
-      searchBar.barTintColor = .white
-    
-      navigationItem.titleView = searchBar
-  }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
@@ -39,4 +21,7 @@ class SearchViewController: UIViewController {
         self.view.endEditing(true)
         
     }
+    
+    
+    
 }
