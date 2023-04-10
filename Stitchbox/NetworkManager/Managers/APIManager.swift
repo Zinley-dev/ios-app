d7add82aab9b168e1b350715a015ad1025129f91
Nghiem Minh Hoang <hoangnm.dev@gmail.com>
2023-04-10 23:06:31 +0700

    update comment

--- Stitchbox/NetworkManager/Managers/APIManager.swift
+++ Stitchbox/NetworkManager/Managers/APIManager.swift
@@ -760,6 +760,10 @@ extension APIManager {
 
 extension APIManager {
   
+  /*
+   params = { "riotUsername": "", "region": "" }
+   
+   */
   func updateRiot(params: [String: Any], completion: @escaping APICompletion) {
     userManager.request(.riotUpdate(params: params)) { result in
       completion(result)
